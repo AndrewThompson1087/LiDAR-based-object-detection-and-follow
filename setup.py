a2a7aaadadb8d137f7df61b3b49f0c09fa8168c3
Andrew Thompson <99454471+AndrewThompson1087@users.noreply.github.com>
2024-11-19 22:26:48 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'project3'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -19,7 +19,6 @@ setup(
     maintainer_email='jasdeepbajaj3@tamu.edu',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': ['ObjectDetectionNode = project3.ObjectDetectionNode:main',
         'ObjectTrackingNode = project3.ObjectTrackingNode:main',
